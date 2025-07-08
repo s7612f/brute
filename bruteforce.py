7ab44088bb15b9cbf05b3f4b23ce72fc8ba5d826
s7612f <75951407+s7612f@users.noreply.github.com>
2025-07-08 01:50:02 +0100

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -3,6 +3,17 @@
 import subprocess
 import argparse
 import json
+import os
+import urllib.request
+
+def download_password_list(url, dest):
+    urllib.request.urlretrieve(url, dest)
+
+def extract_gzip(file_path):
+    import gzip
+    with gzip.open(file_path, 'rb') as f_in:
+        with open(file_path.replace('.gz', ''), 'wb') as f_out:
+            f_out.write(f_in.read())
 
 def load_config(config_file):
     with open(config_file, 'r') as f:
@@ -24,14 +35,23 @@ def main():
     parser.add_argument('config', help='Path to the configuration file')
     args = parser.parse_args()
 
+    # Download and extract the password list
+    password_list_url = 'https://crackstation.net/files/crackstation.txt.gz'
+    password_list_path = os.path.expanduser('~/wordlists/crackstation.txt.gz')
+    os.makedirs(os.path.dirname(password_list_path), exist_ok=True)
+    download_password_list(password_list_url, password_list_path)
+    extract_gzip(password_list_path)
+
+    # Load the configuration
     config = load_config(args.config)
 
     target = config['target']
     username = config['username']
-    password_list = config['password_list']
+    password_list = os.path.expanduser(config['password_list'])
     protocol = config['protocol']
     params = config['params']
 
+    # Run the brute-force attack
     brute_force(target, username, password_list, protocol, params)
 
 if __name__ == '__main__':
